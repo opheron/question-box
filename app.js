6387894d8c88764d198a142b16b7319fb70d3e40
Andrew Chong <opheron@gmail.com>
2017-01-23 16:08:18 -0500

    Add currentUser functionality to header partial

--- app.js
+++ app.js
@@ -32,6 +32,11 @@ passport.use(new passportLocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use(function(req, res, next){
+	res.locals.currentUser = req.user;
+	next();
+});
+
 app.use("/", indexRoutes);
 app.use("/questions", questionRoutes);
 
